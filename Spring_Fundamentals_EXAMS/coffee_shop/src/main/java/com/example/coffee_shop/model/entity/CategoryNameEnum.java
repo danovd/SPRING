5efea5a26e7bb237951d960b2acf5710737cbb7e
danovd <96645752+danovd@users.noreply.github.com>
2023-10-07 11:05:48 +0300

    basic changes, Order entity

---
+++ Spring_Fundamentals_EXAMS/coffee_shop/src/main/java/com/example/coffee_shop/model/entity/CategoryNameEnum.java
@@ -0,0 +1,5 @@
+package com.example.coffee_shop.model.entity;
+
+public enum CategoryNameEnum {
+    COFFEE, CAKE, DRINK, OTHER
+}
---
+++ Spring_Fundamentals_EXAMS/coffee_shop/src/main/java/com/example/coffee_shop/model/entity/Order.java
@@ -0,0 +1,75 @@
+package com.example.coffee_shop.model.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "orders")
+public class Order extends BaseEntity{
+
+    private String name;
+    private String description;
+    private BigDecimal price;
+
+    private LocalDateTime orderTime;
+
+    private Category category;
+
+    private User employee;
+
+    public Order() {
+    }
+    @Column(nullable = false)
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    @Column(nullable = false, columnDefinition = "TEXT")
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    @Column(nullable = false, precision = 19, scale = 2)
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+    @Column(nullable = false)
+    public LocalDateTime getOrderTime() {
+        return orderTime;
+    }
+
+    public void setOrderTime(LocalDateTime orderTime) {
+        this.orderTime = orderTime;
+    }
+    @ManyToOne
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+    @ManyToOne
+    public User getEmployee() {
+        return employee;
+    }
+
+    public void setEmployee(User employee) {
+        this.employee = employee;
+    }
+}
