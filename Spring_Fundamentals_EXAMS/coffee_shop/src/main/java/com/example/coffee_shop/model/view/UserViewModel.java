f2001e50ebed4c5e43bfabfcb1aae5cc6d6f3214
danovd <96645752+danovd@users.noreply.github.com>
2023-10-10 11:23:02 +0300

    User View Model

---
+++ Spring_Fundamentals_EXAMS/coffee_shop/src/main/java/com/example/coffee_shop/model/view/UserViewModel.java
@@ -0,0 +1,25 @@
+package com.example.coffee_shop.model.view;
+
+public class UserViewModel {
+    private String username;
+    private Integer countOfOrders;
+
+    public UserViewModel() {
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Integer getCountOfOrders() {
+        return countOfOrders;
+    }
+
+    public void setCountOfOrders(Integer countOfOrders) {
+        this.countOfOrders = countOfOrders;
+    }
+}
