c7ba65f70867c3b58eb6fccd402ecc6406170413
danovd <96645752+danovd@users.noreply.github.com>
2023-10-10 11:19:00 +0300

    User Register Binding Model

---
+++ Spring_Fundamentals_EXAMS/coffee_shop/src/main/java/com/example/coffee_shop/model/binding/UserRegisterBindingModel.java
@@ -0,0 +1,67 @@
+package com.example.coffee_shop.model.binding;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Size;
+
+public class UserRegisterBindingModel {
+
+    private String username;
+    private String firstName;
+    private String lastName;
+    private String email;
+
+    private String password;
+
+    private String confirmPassword;
+
+    public UserRegisterBindingModel() {
+    }
+    @Size(min = 5, max = 20)
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    @Size(min = 5, max = 20)
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    @Email
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    @Size(min = 3)
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    @Size(min = 3)
+    public String getConfirmPassword() {
+        return confirmPassword;
+    }
+
+    public void setConfirmPassword(String confirmPassword) {
+        this.confirmPassword = confirmPassword;
+    }
+}
