8c3cfa96a2734aaa865bb7770cf22f5ae222e39a
danovd <96645752+danovd@users.noreply.github.com>
2023-10-10 11:18:39 +0300

    User login Binding Model

---
+++ Spring_Fundamentals_EXAMS/coffee_shop/src/main/java/com/example/coffee_shop/model/binding/UserLoginBindingModel.java
@@ -0,0 +1,29 @@
+package com.example.coffee_shop.model.binding;
+
+import jakarta.validation.constraints.Size;
+
+public class UserLoginBindingModel {
+
+
+    private String username;
+    private String password;
+
+    public UserLoginBindingModel() {
+    }
+    @Size(min = 5, max = 20)
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    @Size(min = 3)
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
