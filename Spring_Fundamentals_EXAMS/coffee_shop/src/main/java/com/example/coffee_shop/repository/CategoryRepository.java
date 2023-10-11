cb15f9ccbdee9453ed738509fe63b53d01584309
danovd <96645752+danovd@users.noreply.github.com>
2023-10-11 09:35:54 +0300

    category repository

--- Spring_Fundamentals_EXAMS/coffee_shop/src/main/java/com/example/coffee_shop/repository/CategoryRepository.java
+++ Spring_Fundamentals_EXAMS/coffee_shop/src/main/java/com/example/coffee_shop/repository/CategoryRepository.java
@@ -1,9 +1,14 @@
 package com.example.coffee_shop.repository;
 
 import com.example.coffee_shop.model.entity.Category;
+import com.example.coffee_shop.model.entity.CategoryNameEnum;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+    Optional<Category> findByName(CategoryNameEnum name);
+
 }
