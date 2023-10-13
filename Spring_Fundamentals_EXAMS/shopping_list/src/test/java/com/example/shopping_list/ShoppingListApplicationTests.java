774f3b79ccdea10b77187ba294b05b45c189bae7
danovd <96645752+danovd@users.noreply.github.com>
2023-10-13 13:06:03 +0300

    register

---
+++ Spring_Fundamentals_EXAMS/shopping_list/src/test/java/com/example/shopping_list/ShoppingListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shopping_list;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingListApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
