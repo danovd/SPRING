a9f6443f26c9b492a3c5ef6f34665bd828d047a9
danovd <96645752+danovd@users.noreply.github.com>
2023-10-15 10:01:10 +0300

    home controller, general changes

---
+++ Spring_Fundamentals_EXAMS/shopping_list/src/main/java/com/example/shopping_list/controller/HomeController.java
@@ -0,0 +1,38 @@
+package com.example.shopping_list.controller;
+
+import com.example.shopping_list.model.entity.enums.CategoryName;
+import com.example.shopping_list.service.ProductService;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    private final ProductService productService;
+
+    public HomeController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping("/")
+    public String index(HttpSession httpSession, Model model) {
+
+
+        if(httpSession.getAttribute("user") == null){
+            return "index";
+        }
+
+        model.addAttribute("totalSum", productService.getTotalSum());
+        model.addAttribute("drinks", productService
+                .findAllProductsByCategoryName(CategoryName.DRINK));
+        model.addAttribute("food", productService
+                .findAllProductsByCategoryName(CategoryName.FOOD));
+        model.addAttribute("households", productService
+                .findAllProductsByCategoryName(CategoryName.HOUSEHOLD));
+        model.addAttribute("others", productService
+                .findAllProductsByCategoryName(CategoryName.OTHER));
+        return "home";
+    }
+}
