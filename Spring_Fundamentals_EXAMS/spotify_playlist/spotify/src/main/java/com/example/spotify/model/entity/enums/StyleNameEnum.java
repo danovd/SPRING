5a5f5d16aee2286a63307ddfb8fb9e6bcf8a55fc
danovd <96645752+danovd@users.noreply.github.com>
2023-10-16 09:29:25 +0300

    spotify basic

--- Spring_Fundamentals_EXAMS/spotify_playlist/spotify/src/main/java/com/example/spotify/model/entity/enums/StyleNameEnum.java
+++
@@ -1,5 +0,0 @@
-package com.example.spotify.model.entity.enums;
-
-public enum StyleNameEnum {
-    POP, ROCK, JAZZ
-}
--- Spring_Fundamentals_EXAMS/spotify_playlist/spotify/src/main/java/com/example/spotify/service/StyleService.java
+++
@@ -1,5 +0,0 @@
-package com.example.spotify.service;
-
-public interface StyleService {
-    void initStyles();
-}
