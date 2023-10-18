114482d2896f0d90ecaaf1b35c53cc5a3ddce66b
danovd <96645752+danovd@users.noreply.github.com>
2023-10-18 10:10:50 +0300

    enums

---
+++ Spring_Fundamentals_EXAMS/andrey's/src/main/java/com/example/gira/model/entity/enums/CategoryNameEnum.java
@@ -0,0 +1,5 @@
+package com.example.gira.model.entity.enums;
+
+public enum CategoryNameEnum {
+    SHIRT, DENIM, SHORTS, JACKET
+}
---
+++ Spring_Fundamentals_EXAMS/andrey's/src/main/java/com/example/gira/model/entity/enums/GenderEnum.java
@@ -0,0 +1,5 @@
+package com.example.gira.model.entity.enums;
+
+public enum GenderEnum {
+    MALE, FEMALE
+}
