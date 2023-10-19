6e456a0b6ad233489ccb0247d66eef6c9cb6200f
danovd <96645752+danovd@users.noreply.github.com>
2023-10-19 10:30:33 +0300

    item repository

---
+++ Spring_Fundamentals_EXAMS/andrey's/src/main/java/com/example/gira/repository/ItemRepository.java
@@ -0,0 +1,12 @@
+package com.example.gira.repository;
+
+import com.example.gira.model.entity.Item;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ItemRepository extends JpaRepository<Item, String> {
+    Optional<Item> findByName(String name);
+}
