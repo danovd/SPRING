6a5c8a817b285ed51148d747f96af1475b0485d7
danovd <96645752+danovd@users.noreply.github.com>
2023-10-20 14:16:04 +0300

    base entity

---
+++ Spring_Fundamentals_EXAMS/battleships/src/main/java/com/example/battleships/model/entity/BaseEntity.java
@@ -0,0 +1,24 @@
+package com.example.battleships.model.entity;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import org.hibernate.annotations.GenericGenerator;
+
+@MappedSuperclass
+public abstract class BaseEntity {
+    private String id;
+
+    public BaseEntity() {
+    }
+    @Id
+    @GeneratedValue(generator = "uuid-string")
+    @GenericGenerator(name = "uuid-string", strategy = "org.hibernate.id.UUIDGenerator")
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
