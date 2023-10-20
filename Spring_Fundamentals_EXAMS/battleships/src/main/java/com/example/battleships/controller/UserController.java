a3c2d1e5198c793f7691f1dcd14171c4f57585f4
danovd <96645752+danovd@users.noreply.github.com>
2023-10-20 14:15:07 +0300

    user controller

---
+++ Spring_Fundamentals_EXAMS/battleships/src/main/java/com/example/battleships/controller/UserController.java
@@ -0,0 +1,134 @@
+package com.example.battleships.controller;
+
+import com.example.battleships.model.binding.UserLoginBindingModel;
+import com.example.battleships.model.binding.UserRegisterBindingModel;
+import com.example.battleships.model.service.UserServiceModel;
+import com.example.battleships.service.UserService;
+import com.example.battleships.util.CurrentUser;
+import jakarta.validation.Valid;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+@Controller
+@RequestMapping("/users")
+public class UserController {
+
+    private final UserService userService;
+    private final ModelMapper modelMapper;
+    private final CurrentUser currentUser;
+
+    public UserController(UserService userService, ModelMapper modelMapper, CurrentUser currentUser) {
+        this.userService = userService;
+        this.modelMapper = modelMapper;
+        this.currentUser = currentUser;
+    }
+
+
+
+    @ModelAttribute
+    public UserRegisterBindingModel userRegisterBindingModel(){
+        return new UserRegisterBindingModel();
+    }
+
+
+
+
+
+    @GetMapping("/register")
+    public String register(){
+
+        if(currentUser.getId() != null){
+            return "redirect:/";
+        }
+        return "register";
+    }
+
+
+
+
+    @PostMapping("/register")
+    public String registerConfirm(@Valid UserRegisterBindingModel userRegisterBindingModel,
+                                  BindingResult bindingResult, RedirectAttributes redirectAttributes){
+
+        if(currentUser.getId() != null){
+            return "redirect:/";
+        }
+
+        if(bindingResult.hasErrors() || !userRegisterBindingModel.getPassword().equals(userRegisterBindingModel.getConfirmPassword())){
+            redirectAttributes.addFlashAttribute("userRegisterBindingModel", userRegisterBindingModel);
+            redirectAttributes.addFlashAttribute("org.springframework.validation.BindingResult.userRegisterBindingModel", bindingResult);
+
+            return "redirect:register";
+        }
+
+        userService.registerUser(modelMapper.map(userRegisterBindingModel, UserServiceModel.class));
+        return "redirect:login";
+    }
+
+
+
+    @GetMapping("/login")
+    public String login(Model model){
+        // Additional check in case of logged user who enter that path and try to access login - it is guest only
+
+        if(currentUser.getId() != null){
+            return "redirect:/";
+        }
+
+        if(!model.containsAttribute("userLoginBindingModel")){
+            model.addAttribute("userLoginBindingModel", new UserLoginBindingModel());
+            model.addAttribute("notFound", false);
+        }
+        return "login";
+    }
+
+
+
+    @PostMapping("/login")
+    public String loginConfirm(@Valid UserLoginBindingModel userLoginBindingModel,
+                               BindingResult bindingResult,
+                               RedirectAttributes redirectAttributes){
+        // Additional check in case of logged user who enter that path and try to access login - it is guest only
+        if(currentUser.getId() != null){
+            return "redirect:/";
+        }
+
+        if(bindingResult.hasErrors()){
+            redirectAttributes.addFlashAttribute("userLoginBindingModel", userLoginBindingModel);
+            redirectAttributes.addFlashAttribute("org.springframework.validation.BindingResult.userLoginBindingModel", bindingResult);
+
+            return "redirect:login";
+        }
+
+        UserServiceModel userServiceModel =  userService.findByUsernameAndPassword(userLoginBindingModel.getUsername(), userLoginBindingModel.getPassword());
+
+        // user not found
+        if(userServiceModel == null){
+            redirectAttributes.addFlashAttribute("userLoginBindingModel", userLoginBindingModel);
+            redirectAttributes.addFlashAttribute("notFound", true);
+            return "redirect:login";
+        }
+
+        userService.loginUser(userServiceModel.getId(), userServiceModel.getUsername());
+
+        return "redirect:/";
+    }
+
+
+
+
+    @GetMapping("/logout")
+    public String logout(){
+        userService.logout();
+        return "redirect:/";
+    }
+
+
+}
