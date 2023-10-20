6a528ec42b18dd5ca9cda47bf437e2504a498920
danovd <96645752+danovd@users.noreply.github.com>
2023-10-20 14:16:19 +0300

    Category entity

---
+++ Spring_Fundamentals_EXAMS/battleships/src/main/java/com/example/battleships/model/entity/Category.java
@@ -0,0 +1,31 @@
+package com.example.battleships.model.entity;
+
+import com.example.battleships.model.entity.enums.CategoryNameEnum;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "categories")
+public class Category extends BaseEntity{
+
+    private CategoryNameEnum name;
+    private String description;
+
+    public Category() {
+    }
+    @Enumerated(EnumType.STRING)
+    public CategoryNameEnum getName() {
+        return name;
+    }
+
+    public void setName(CategoryNameEnum name) {
+        this.name = name;
+    }
+    @Column(name="description", columnDefinition = "TEXT")
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
