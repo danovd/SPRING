00bb63e72c7f1f5c9caf52d9afddd2b755600a4f
danovd <96645752+danovd@users.noreply.github.com>
2023-10-25 10:43:08 +0300

    user repository

---
+++ Spring_Fundamentals_EXAMS/gira/src/main/java/com/example/gira/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.example.gira.repository;
+
+import com.example.gira.model.entity.User;
+import io.micrometer.observation.ObservationFilter;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, String> {
+    Optional<User> findByEmailAndPassword(String email, String password);
+}
