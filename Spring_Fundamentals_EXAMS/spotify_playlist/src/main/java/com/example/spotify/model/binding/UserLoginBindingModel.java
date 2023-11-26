c96722e53894c46fd29dc884967507dd78169b5b
danovd <96645752+danovd@users.noreply.github.com>
2023-11-26 10:39:48 +0200

    user login binding model

---
+++ Spring_Fundamentals_EXAMS/spotify_playlist/src/main/java/com/example/spotify/model/binding/UserLoginBindingModel.java
@@ -0,0 +1,31 @@
+package com.example.spotify.model.binding;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public class UserLoginBindingModel {
+
+    private String username;
+    private String password;
+
+    public UserLoginBindingModel() {
+    }
+    @NotBlank(message = "Username cannot be empty string")
+    @Size(min = 3, max = 20, message = "Username length is allowed to be from 3 to 20 characters")
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    @NotBlank(message = "Password cannot be empty string")
+    @Size(min = 3, max = 20, message = "Password length is allowed to be from 3 to 20 characters")
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
