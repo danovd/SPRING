094e91c4f765f2202f965ae609c16b8eb2906c02
danovd <96645752+danovd@users.noreply.github.com>
2023-11-30 09:35:33 +0200

    home controller

--- Spring_Fundamentals_EXAMS/heroes/src/main/java/project/init/DatabaseInitializer.java
+++ Spring_Fundamentals_EXAMS/heroes/src/main/java/project/init/DatabaseInitializer.java
@@ -1,1 +1,24 @@
+package project.init;
+
+
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DatabaseInitializer implements CommandLineRunner{
+
+/*
+    private final CategoryService categoryService;
+
+    public DatabaseInitializer(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
+
 */
+    @Override public void run(String... args) throws Exception {
+  //     categoryService.initMoods();
+    }
+}
+
