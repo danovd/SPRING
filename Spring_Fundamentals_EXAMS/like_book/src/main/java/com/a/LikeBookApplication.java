42a084a780c7f5373d9d64f9b20eba0cb89db2e2
danovd <96645752+danovd@users.noreply.github.com>
2024-01-05 20:04:49 +0200

    like book app

---
+++ Spring_Fundamentals_EXAMS/like_book/src/main/java/com/a/LikeBookApplication.java
@@ -0,0 +1,13 @@
+package com.a;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LikeBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LikeBookApplication.class, args);
+	}
+
+}
